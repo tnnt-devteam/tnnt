c94709d29b40998649c4eb1eb062dd6a4fedd8fe
copperwater <aosdict@gmail.com>
2019-10-17 22:15:23 -0400

    Create the NPC in the arena

--- src/sp_lev.c
+++ src/sp_lev.c
@@ -1642,8 +1642,12 @@ struct mkroom *croom;
 
     if (m->align != -(MAX_REGISTERS + 2))
         mtmp = mk_roamer(pm, Amask2align(amask), x, y, m->peaceful);
-    else if (PM_ARCHEOLOGIST <= m->id && m->id <= PM_WIZARD)
-        mtmp = mk_mplayer(pm, x, y, FALSE);
+    else if (PM_ARCHEOLOGIST <= m->id && m->id <= PM_WIZARD) {
+        if (Is_deathmatch_level(&u.uz))
+            create_tnnt_npc(x, y);
+        else
+            mtmp = mk_mplayer(pm, x, y, FALSE);
+    }
     else
         mtmp = makemon(pm, x, y, NO_MM_FLAGS);
 
