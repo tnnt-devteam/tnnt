9f42e9124ff8ed32816cf67cc4cdee70818a3b59
Michael Meyer <me@entrez.cc>
2022-10-03 14:28:30 -0400

    Escape and quote YAML strings a different way
    To avoid the issue with parsing achievement names and descriptions that
    start with a '#' character, without reviving the issue with strings that
    have been broken up into multiple constants, use quoted strings that
    have been escaped 'by hand' with a new function.

--- util/tnnt_ach_to_yaml.c
+++ util/tnnt_ach_to_yaml.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+#define DESC_SIZE 256
+
 #define SIZE(x) (int) (sizeof (x) / sizeof (x[0]))
+
+const char *escape_string(const char *s);
 
 #define ACH(nam, desc, id) { nam, desc }
 struct ach_info {
@@ -8,7 +12,6 @@ struct ach_info {
 } ach_list[] = {
 #include "tnnt_achievements.h"
 };
-#undef ACH
 
 int
 main(void)
@@ -18,10 +21,34 @@ main(void)
         struct ach_info ach = ach_list[i];
         int lfnum = i / 64, lfbit = i % 64;
         printf("- model: scoreboard.achievement\n  fields:\n");
-        printf("    name: %s\n    description: %s\n", ach.name, ach.desc);
-        printf("    xlogfield: tnntachieve%d\n    bit: %d\n", lfnum,
+        printf("    name: \"%s\"\n", escape_string(ach.name));
+        printf("    description: \"%s\"\n", escape_string(ach.desc));
+        printf("    xlogfield: \"tnntachieve%d\"\n    bit: %d\n", lfnum,
                lfbit);
     }
 
     return 0;
+}
+
+/* escape the string so it can be quoted in the YAML file.
+ * ex: input:  "Tame seven \"f\"" [prints as: Tame seven "f"]
+ *     output: "Tame seven \\\"f\\\"" [prints as: Tame seven \"f\"]
+ */
+const char *
+escape_string(const char *s)
+{
+    static char result[DESC_SIZE];
+    int i, j;
+
+    /* copy 's' to 'result' a char at a time */
+    for (i = j = 0; s[i] != '\0' && j < DESC_SIZE - 1; i++) {
+        /* prepend a backslash '\' before any characters that should be
+           escaped in the resulting string (just quotes for now) */
+        if (s[i] == '"') {
+            result[j++] = '\\';
+        }
+        result[j++] = s[i];
+    }
+    result[j] = '\0';
+    return result;
 }
