95d5e2f4e596cd426511f8dd48b0e4a33c5d1765
k21971 <keith.simpson1971@gmail.com>
2026-05-02 20:28:20 +0000

    Fix: config.h was pointing to the wrong extrainfo type.

--- include/config.h
+++ include/config.h
@@ -658,7 +658,7 @@ typedef unsigned char uchar;
    dgamelaunch-based server play */
 /* #define DGAMELAUNCH */
 #ifdef DGAMELAUNCH
-#define EXTRAINFO_FN    "/dgldir/extrainfo-nh370/%n.extrainfo"
+#define EXTRAINFO_FN    "/dgldir/extrainfo-nh500/%n.extrainfo"
 #define MAILCKFREQ 5    /* SIMPLE_MAIL is in unixconf.h */
 #define WHEREIS_FILE    "whereis/%n.whereis" /* Write out player's current location to player.whereis */
 
