083e55e905699d8a73aa2b77b22b4030b6e8bd01
nhmall <nhmall@nethack.org>
2026-05-16 12:22:03 -0400

    Windows curses-related follow-up

--- include/extern.h
+++ include/extern.h
@@ -2032,10 +2032,6 @@ extern int win32_cr_helper(char, struct CRctxt *, void *, int);
 extern int win32_cr_gettrace(int, char *, int);
 extern int *win32_cr_shellexecute(const char *);
 # endif
-#ifdef MSWIN_GRAPHICS
-extern int get_approx_display_width(void);
-extern int get_approx_display_rows(void);
-#endif
 #endif /* WIN32 */
 
 #endif /* MICRO || WIN32 */
@@ -2172,7 +2168,7 @@ extern void tutorial(boolean);
 
 #if !defined(MAKEDEFS_C) && !defined(MDLIB_C) && !defined(CPPREGEX_C)
 
-/* ### consoletty.c ### */
+/* ### consoletty.c  ### */
 
 #ifdef WIN32
 extern void get_scr_size(void);
@@ -2185,6 +2181,7 @@ void console_g_putch(int in_ch);
 extern void set_output_mode(int);
 extern void synch_cursor(void);
 extern void nethack_enter_consoletty(void);
+/* body in consoletty.c and mhmain.c */
 extern int get_approx_display_cols(void);
 extern int get_approx_display_rows(void);
 extern void console_exit(void);
